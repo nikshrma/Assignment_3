636ba8be7f507850e3f91bd3f233b7628373c719
nikshrma <nikshrma2006@gmail.com>
2024-10-29 22:28:59 +0530

    A2Q12

---
+++ A2Q12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    //declaration
+    int n = 20 , count=0 ,num ;
+    //loop
+    while(n>0){
+        //input numbers
+        printf("Enter number %d\n" ,21-n);
+        scanf("%d" , &num);
+        //count goes up by 1 every time an even number is encountered
+        if(num%2==0){
+            count++;
+        }
+        n--;
+    }
+    //output
+    printf("The number of even numbers is %d\n" , count);
+    return 0;
+}
